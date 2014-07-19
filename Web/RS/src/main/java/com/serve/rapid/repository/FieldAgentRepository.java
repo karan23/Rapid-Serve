6dd69807eeaf2c2deb40916e5c1b8c9a9ac2a9d9
Sorabh Saluja <sorabhsaluja@gmail.com>
2014-07-19 17:26:54 +0530

    Domain Objects and Configuration Classes

---
+++ Web/RS/src/main/java/com/serve/rapid/repository/FieldAgentRepository.java
@@ -0,0 +1,13 @@
+package com.serve.rapid.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.serve.rapid.domain.FieldAgent;
+public interface FieldAgentRepository extends CrudRepository<FieldAgent, Long>{
+	
+	
+	List<FieldAgent> findByName(String name);
+		
+}
