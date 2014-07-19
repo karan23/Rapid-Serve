914ccce571ba8d6c6e09c71465c716162cba3005
ani-bellurbis <aniruddh@bellurbis.com>
2014-07-20 02:40:35 +0530

    data class added

--- Android/RapidServeField/src/com/example/rapidserve/OpenComplaints.java
+++ Android/RapidServeField/src/com/example/rapidserve/OpenComplaints.java
@@ -1,5 +1,7 @@
 package com.example.rapidserve;
 
+import com.example.rapidserve.RestClient.RequestMethod;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,8 +21,35 @@ public View getView() {
 		// TODO Auto-generated method stub
     View rootView = inflater.inflate(
             R.layout.opencomplaint, container, false);
-    Bundle args = getArguments();
+    
 
 		return rootView;
 	}
+
+
+private void callWebservice() {
+
+	Thread t = new Thread(new Runnable() {
+		
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			String url = utils.APP_URL + "getAllComplaintByAgentId/" + utils.getAppParam(getActivity(), "nameFeildId");
+			String resp = "";
+			RestClient restClient = new RestClient(url);
+			try {
+				restClient.Execute(RequestMethod.GET);
+				resp = restClient.getResponse();
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+
+			}
+		}
+	});
+
+
+}
+
 }
