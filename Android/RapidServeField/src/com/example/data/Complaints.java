d34bbd6566321dfe7654c6f9fa52056d05f3c627
ani-bellurbis <aniruddh@bellurbis.com>
2014-07-20 02:22:25 +0530

    pull commit

---
+++ Android/RapidServeField/src/com/example/data/Complaints.java
@@ -0,0 +1,15 @@
+package com.example.data;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Complaints {
+
+int id ;
+String type;
+String detail;
+String userName;
+String Adress ;
+LatLng latlng ;
+long phoneNumber ;
+
+}
