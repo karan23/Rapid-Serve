94acdf431a60d8f7a27000f8228adbb7a9ec2bb7
ani-bellurbis <aniruddh@bellurbis.com>
2014-07-19 20:41:57 +0530

    empty fragment added

--- Android/RapidServeField/src/com/example/rapidserve/MainActivity.java
+++ Android/RapidServeField/src/com/example/rapidserve/MainActivity.java
@@ -1,23 +1,97 @@
 package com.example.rapidserve;
 
-import android.app.Activity;
+import android.app.ActionBar.Tab;
 import android.app.ActionBar;
-import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.Window;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
+	AppSectionsPagerAdapter mAppSectionsPagerAdapter;
+	public ViewPager mViewPager;
+	protected ActionBar actionBar;// = getActionBar();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
 		setContentView(R.layout.activity_main);
+		mAppSectionsPagerAdapter = new AppSectionsPagerAdapter(getSupportFragmentManager());
 
+		// Set up the action bar.
+		actionBar = getActionBar();
+
+		// Specify that the Home/Up button should not be enabled, since there is
+		// no hierarchical
+		// parent.
+		actionBar.setHomeButtonEnabled(false);
+
+		// Specify that we will be displaying tabs in the action bar.
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		mViewPager = (ViewPager) findViewById(R.id.pager);
+		mViewPager.setAdapter(mAppSectionsPagerAdapter);
+		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+
+			@Override
+			public void onPageSelected(int position) {
+				// When swiping between different app sections, select the
+				// corresponding tab.
+				// We can also use ActionBar.Tab#select() to do this if we have
+				// a reference to the
+				// Tab.
+				actionBar.setSelectedNavigationItem(position);
+
+			}
+		});
+
+		actionBar.addTab(actionBar.newTab().setText("Open Issues").setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setText("Close Issues").setTabListener(this));
+		actionBar.addTab(actionBar.newTab().setText("Reports").setTabListener(this));
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+		mViewPager.setCurrentItem(tab.getPosition());
+		mAppSectionsPagerAdapter.notifyDataSetChanged();
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public class AppSectionsPagerAdapter extends FragmentStatePagerAdapter {
+
+		public AppSectionsPagerAdapter(FragmentManager fragmentManager) {
+			super(fragmentManager);
+		}
+
+		@Override
+		public int getCount() {
+			return 3;
+		}
+
+		@Override
+		public android.support.v4.app.Fragment getItem(int arg0) {
+			// TODO Auto-generated method stub
+			Fragment fragment = new OpenComplaints();
+
+			return fragment;
+		}
 
 	}
 
