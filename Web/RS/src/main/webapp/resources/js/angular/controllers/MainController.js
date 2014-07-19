5b4d7cd41602f09ef9847779ede2aa9e8845d1fc
Sorabh Saluja <sorabhsaluja@gmail.com>
2014-07-19 22:37:13 +0530

    UI for Adding Field Agent

--- Web/RS/src/main/webapp/resources/js/angular/controllers/MainController.js
+++ Web/RS/src/main/webapp/resources/js/angular/controllers/MainController.js
@@ -6,6 +6,20 @@
  * @constructor
  */
 var MainCtrl = function($scope, toaster, $location,
-		AppServices, $filter, $route, $routeParams ) {
+		AppServices, $filter, $route, $routeParams,$http ) {
+	
+	$scope.fa = {};
+	
+	$scope.saveFA = function(){
+		console.log($scope.fa);
+		var responsePromise = $http.post("fa/addFieldAgent", $scope.fa);
+
+        responsePromise.success(function(data, status, headers, config) {
+            $scope.myData.fromServer = data.title;
+        });
+        responsePromise.error(function(data, status, headers, config) {
+            alert("AJAX failed!");
+        });
+	};
 	
 };
