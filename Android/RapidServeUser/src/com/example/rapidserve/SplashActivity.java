fa6eca2ea610124b30b560a4e2ea73e78b9f3b06
unknown <yogeshbalan@live.co.uk>
2014-07-20 13:42:40 +0530

    final commit UI

--- Android/RapidServeUser/src/com/example/rapidserve/SplashActivity.java
+++ Android/RapidServeUser/src/com/example/rapidserve/SplashActivity.java
@@ -2,10 +2,13 @@ package com.example.rapidserve;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.TextView;
 
 import com.rapidserve.user.R;
+import com.rapidserve.user.R.id;
 
 public class SplashActivity extends Activity {
 	private Intent mStartIntent;
@@ -16,7 +19,7 @@ public class SplashActivity extends Activity {
 		
 		setContentView(R.layout.splashscreen);
 		
-		
+
 		// Send a message in 3.5 sec to start Home Page
 	    new Handler().postDelayed(new Runnable(){ 
 	    	public void run() {
