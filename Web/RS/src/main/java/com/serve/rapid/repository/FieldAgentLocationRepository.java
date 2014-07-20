bdf6d9ccc521f9f6d7cc1878c6a16ac447b6f030
gaurav singh <grv341@gmail.com>
2014-07-20 07:12:47 +0530

    fixed issues

--- Web/RS/src/main/java/com/serve/rapid/repository/FieldAgentLocationRepository.java
+++ Web/RS/src/main/java/com/serve/rapid/repository/FieldAgentLocationRepository.java
@@ -1,9 +1,13 @@
 package com.serve.rapid.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.serve.rapid.domain.FieldAgentLocation;
 
 public interface FieldAgentLocationRepository extends CrudRepository<FieldAgentLocation, Long> {
+
+	List<FieldAgentLocation> findAllById(long parseLong);
 
 }
